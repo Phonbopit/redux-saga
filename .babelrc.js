3db7281cbd464cd7e332047b24bfd9298cb17e34
Mateusz Burzyński <mateuszburzynski@gmail.com>
2017-09-20 01:13:50 +0200

    Migrated to Babel7 and Webpack3

---
+++ .babelrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    [
+      'env',
+      {
+        loose: true,
+        modules: process.env.BABEL_ENV === 'cjs' ? 'commonjs' : false,
+        forceAllTransforms: process.env.NODE_ENV === 'production',
+      },
+    ],
+    'react',
+    'stage-2',
+  ],
+}
